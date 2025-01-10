e514273e0286a9fdec1c5b35dbc29b36420efea7
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-01-10 13:58:17 -0300

    created project backend

---
+++ produtosBackend/src/main/java/io/gitHub/AugustoMello09/oak/OakApplication.java
@@ -0,0 +1,13 @@
+package io.gitHub.AugustoMello09.oak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakApplication.class, args);
+	}
+
+}
