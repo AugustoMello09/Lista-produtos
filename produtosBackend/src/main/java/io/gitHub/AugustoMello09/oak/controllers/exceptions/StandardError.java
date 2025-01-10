59f9e22251f6c0bb9f9d8acba1d8b5e0f194e563
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-01-10 15:08:08 -0300

    feat: implement custom exception handling

---
+++ produtosBackend/src/main/java/io/gitHub/AugustoMello09/oak/controllers/exceptions/StandardError.java
@@ -0,0 +1,20 @@
+package io.gitHub.AugustoMello09.oak.controllers.exceptions;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private LocalDateTime timestemp;
+	private Integer status;
+	private String error;
+	private String path;
+}
