b56ae723538ccbcddfce42d841608339d33ac00f
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-01-13 14:13:38 -0300

    feat : add header component

--- produtosFrontend/src/app/app.module.ts
+++ produtosFrontend/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './core/home/home.module';
+import { SharedModule } from './core/shared/shared.module';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +16,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    SharedModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- produtosFrontend/tailwind.config.js
+++ produtosFrontend/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'dm': ['DM Sans', 'sans-serif'],
+        'Gro' : ['Space Grotesk', 'sans-serif']
+      },
+      screens: {
+        'auto1': {'max' : '1280px'},
+        'auto': {'max' : '1024px'},
+        'menor': {'max' : '824px'},
+        'xr': {'max' : '599px'},
+        'xd': { 'max': '409px' },
+        'xdd': { 'max': '320px' }, 
+      }
+    },
   },
   plugins: [],
 }
