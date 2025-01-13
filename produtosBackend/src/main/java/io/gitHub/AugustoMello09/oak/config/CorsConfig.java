77c8d0b5e49c82c5d81ee5e049d653f0e5e4295c
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-01-13 10:10:44 -0300

    feat : Cors configuration

---
+++ produtosBackend/src/main/java/io/gitHub/AugustoMello09/oak/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package io.gitHub.AugustoMello09.oak.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+
+	@Bean
+	CorsFilter corsFilter() {
+		return new CorsFilter(corsConfigurationSource());
+	}
+
+}
