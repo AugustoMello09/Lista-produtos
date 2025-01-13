d5e14b92d34bc79d5976ec2d34ea9921abbc1518
José Augusto Mello Crispim <101072311+AugustoMello09@users.noreply.github.com>
2025-01-13 14:15:22 -0300

    feat : add Footer

--- produtosFrontend/src/app/core/shared/shared.module.ts
+++ produtosFrontend/src/app/core/shared/shared.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material.module';
 import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
     MaterialModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
